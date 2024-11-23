d144c3b670568f95daa9e0537447ad20fc73dcc6
hasibayoubi <82474817+hasibayoubi@users.noreply.github.com>
2024-11-23 12:31:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function handleScrollAnimations() {
+  const sections = document.querySelectorAll("section");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  });
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", handleScrollAnimations);
